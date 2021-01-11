e2fd033277133cd0e07ed738229e98c079a904d2
Mike Hamer <mhamer@bandwidth.com>
2021-01-11 15:00:45 -0500

    Add interface items and actions

--- WebRTCAudioCallStoryboard/WebRTCAudioCallStoryboard/ViewController.swift
+++ WebRTCAudioCallStoryboard/WebRTCAudioCallStoryboard/ViewController.swift
@@ -8,12 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var callButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func connect(_ sender: Any) {
+    
+    }
+    
+    @IBAction func call(_ sender: Any) {
+        
+    }
 }
 
