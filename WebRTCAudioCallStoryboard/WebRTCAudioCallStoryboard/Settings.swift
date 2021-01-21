4108826f2b4db943b7ef844676289a8c700ffa79
Mike Hamer <mhamer@bandwidth.com>
2021-01-21 13:28:55 -0500

    Update failure messages and button placement

--- WebRTCAudioCallStoryboard/WebRTCAudioCallStoryboard/Settings.swift
+++ WebRTCAudioCallStoryboard/WebRTCAudioCallStoryboard/Settings.swift
@@ -13,7 +13,7 @@ class Settings {
     /// Address contains the path to your application server.
     var address: String = {
         guard let path = Bundle.main.path(forResource: "Config", ofType: "plist"), let data = try? Data(contentsOf: URL(fileURLWithPath: path)) else {
-            fatalError("Failed to load configuration property list. Did you remember to create a Config.plist file?")
+            fatalError("Failed to load configuration property list. Did you create a Config.plist file?")
         }
         
         guard let configuration = try? PropertyListSerialization.propertyList(from: data, options: .mutableContainers, format: nil) as? [String: String] else {
@@ -21,7 +21,7 @@ class Settings {
         }
         
         guard let address = configuration["Address"] else {
-            fatalError("Failed to load address. Did you remember to add an Address key with the value of your application server address?")
+            fatalError("Failed to load address. Did you add an Address key with your application server address?")
         }
         
         return address
